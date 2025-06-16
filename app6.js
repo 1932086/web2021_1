5eddcff2adb046ab942d18b6ac284d315c81ffaa
1932086 <s1932086jl@s.chibakoudai.jp>
2025-06-16 10:56:56 +0900

    20250616最終

--- app6.js
+++ app6.js
@@ -2,7 +2,44 @@ const express = require("express");
 const app = express();
 
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('test.db');
+const db = new sqlite3.Database('test2.db');
+
+let sql = `
+select car.id, car.name, maker.name as name2
+from car inner join maker
+on car.maker_id=maker.id;
+`
+
+let sql2 = `
+select streamer.id, streamer.name, office.name as name2
+from streamer inner join office
+on streamer.office_id=office.id;
+`
+
+db.serialize( () => {
+    db.all( sql, (error, row) => {
+        if(error) {
+            console.log('Error: ', error );
+            return;
+        }
+        for( let data of row ) {
+            console.log( data.id + ' : ' + data.name + ' : ' + data.name2 );
+        }
+    });
+});
+
+db.serialize( () => {
+    db.all( sql2, (error, row) => {
+        if(error) {
+            console.log('Error: ', error );
+            return;
+        }
+        for( let data of row ) {
+            console.log( data.id + ' : ' + data.name + ' : ' + data.name2 );
+        }
+    });
+});
+
 
 app.set('view engine', 'ejs');
 app.use("/public", express.static(__dirname + "/public"));
@@ -38,6 +75,8 @@ app.get("/top", (req, res) => {
         })
     })
 })
+
+
 app.use(function(req, res, next) {
   res.status(404).send('ページが見つかりません');
 });
