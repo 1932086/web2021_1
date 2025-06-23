440c814544ee58aec2fe882649f6fd7441566da5
1932086 <s1932086jl@s.chibakoudai.jp>
2025-06-23 12:21:08 +0900

    20250623編集

--- app6.js
+++ app6.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('test2.db');
+const db = new sqlite3.Database('test.db');
+const db2 = new sqlite3.Database('test2.db');
+
 
 let sql = `
 select car.id, car.name, maker.name as name2
@@ -16,8 +18,8 @@ from streamer inner join office
 on streamer.office_id=office.id;
 `
 
-db.serialize( () => {
-    db.all( sql, (error, row) => {
+db2.serialize( () => {
+    db2.all( sql, (error, row) => {
         if(error) {
             console.log('Error: ', error );
             return;
@@ -28,8 +30,8 @@ db.serialize( () => {
     });
 });
 
-db.serialize( () => {
-    db.all( sql2, (error, row) => {
+db2.serialize( () => {
+    db2.all( sql2, (error, row) => {
         if(error) {
             console.log('Error: ', error );
             return;
@@ -43,6 +45,8 @@ db.serialize( () => {
 
 app.set('view engine', 'ejs');
 app.use("/public", express.static(__dirname + "/public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   const message = "Hello world";
@@ -56,6 +60,16 @@ app.get("/db", (req, res) => {
                 res.render('show', {mes:"エラーです"});
             }
             res.render('select', {data:row});
+        })
+    })
+})
+app.get('/db/:id', (req,res) => {
+    db.serialize( () => {
+        db.all('select id, 都道府県, 人口, 大学 from example where id=' + req.params.id + ';', (error, row) => {
+            if( error ) {
+                res.render('show', {mes:'エラーです'});
+            }
+            res.render('db', {data:row});
         })
     })
 })
@@ -75,7 +89,39 @@ app.get("/top", (req, res) => {
         })
     })
 })
+app.get("/top2", (req, res) => {
+    //console.log(req.query.pop);    // ①
+    let desc = "";
+    if( req.query.desc ) desc = " desc";
+    let sql = "select id, 都道府県, 人口";
+    //console.log(sql);    // ②
+    db.serialize( () => {
+        db.all(sql, (error, data) => {
+            if( error ) {
+                res.render('show', {mes:"エラーです"});
+            }
+            //console.log(data);    // ③
+            res.render('select', {data:data});
+        })
+    })
+})
 
+app.post('/insert', (req, res) => {
+    let sql = `
+    insert into example (都道府県,人口,大学) values ('` + req.body.name + `',` + req.body.jinko + `,` + req.body.daigaku + `);
+    `
+    console.log(sql);
+    db.serialize( () => {
+        db.run( sql, (error, row) => {
+            console.log(error);
+            if(error) {
+                res.render('show', {mes:'エラーです'});
+            }
+            res.redirect('/db');
+        });
+    });
+    console.log(req.body);
+});
 
 app.use(function(req, res, next) {
   res.status(404).send('ページが見つかりません');
